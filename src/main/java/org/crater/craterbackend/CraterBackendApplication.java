f651526e09282e9dc950082af9f544f421fcb76e
Drew Nollsch <drew.nollsch@gmail.com>
2021-10-26 14:02:14 +0200

    Initial commit

---
+++ src/main/java/org/crater/craterbackend/CraterBackendApplication.java
@@ -0,0 +1,13 @@
+package org.crater.craterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/crater/craterbackend/CraterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crater.craterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
