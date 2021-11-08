d855b7b37245a49066cb248b271691f1b5b357eb
Drew Nollsch <drew.nollsch@gmail.com>
2021-11-08 17:16:32 +0100

    added carbon design system;

---
+++ frontend/src/app.js
@@ -0,0 +1,17 @@
+import { Button } from 'carbon-components-react';
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <Button>Hello React!</Button>
+                </header>
+            </div>
+
+
+
+        );
+    }
+}
