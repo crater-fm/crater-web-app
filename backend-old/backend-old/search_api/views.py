b0cc28bfbdc9ce0fac640a506cecf2ea91f4a9f3
Drew Nollsch <drew.nollsch@gmail.com>
2021-11-17 20:38:42 +0100

    removed backend-old files

--- backend-old/backend-old/search_api/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, welcome to the Crater API.")
--- backend-old/backend-old/views.py
+++
@@ -1,5 +0,0 @@
-rom django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, welcome to the Crater Homepage.")
