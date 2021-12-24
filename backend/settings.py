d7e0cd98057dbd8ff9cf9e5d8bb2abb99b74dac5
Drew Nollsch <drew.nollsch@gmail.com>
2021-12-23 17:00:45 -0800

    changed to page number pagination styyle

--- backend/settings.py
+++ backend/settings.py
@@ -148,7 +148,7 @@ CSRF_TRUSTED_ORIGINS = env('DJANGO_ALLOWED_ORIGINS').split()
 
 # Pagination settings
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
 
