4db4bd853d6c82bb050520d63f4bea2c7130e691
Drew Nollsch <drew.nollsch@gmail.com>
2022-01-11 00:05:25 +0100

    added root path view so that AWS wont show the service as degraded

--- crater_api/urls.py
+++ crater_api/urls.py
@@ -3,6 +3,7 @@ from crater_api import views
 from crater_api.views import ArtistListPlayCount, DjListEpisodeCount, EpisodeList
 
 urlpatterns = [
+    path('', EpisodeList.as_view(),  # just so AWS won't show root path as Degraded state
     path('api/artist', ArtistListPlayCount.as_view()),
     path('api/artist/<int:artist_id>', views.artist_details),
     path('api/artist/name/<keyword>', views.artist_name_contains),
