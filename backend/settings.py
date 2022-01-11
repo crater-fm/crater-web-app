7749e2656b91d188344016b798fa4640783fcb2f
Drew Nollsch <drew.nollsch@gmail.com>
2022-01-11 15:41:27 +0100

    added drewnollsch.com to allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -179,4 +179,7 @@ if DEBUG is True:
   INTERNAL_IPS = AllIPs()
 
 # Add AWS Elastic Beanstalk to ALLOWED_HOSTS
-ALLOWED_HOSTS = ['crater-api.eu-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = [
+    'crater-api.eu-west-2.elasticbeanstalk.com',
+    'crater-api.drewnollsch.com'
+    ]
